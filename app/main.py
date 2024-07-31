30cffaad5ad3fc8bf4e19d115e31e77cadf4bc4a
parit14 <toshiekaushik@gmail.com>
2024-07-30 17:51:09 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -16,27 +16,27 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         plummbing_flag = sys.argv[2]
-        if plummbing_flag == "p":
-            blob_id = sys.argv[3]
-            blob_dir = "".join(blob_id[:2])
-            path = ".git/objects/" + blob_dir + "/" + "".join(blob_id[2:])
-            with open(path, "rb") as f:
-                decompress_blob = str(zlib.decompress(f.read()))
-                vals = decompress_blob.split(" ")
-                ans = []
-                for idx, val in enumerate(vals):
-                    if "\\x00" in val:
-                        vs = val.split("\\x00")
-                        ans.append(vs[-1])
-                        ans += vals[idx+1:]
-                        break
-                a = ""
-                for letter in ans[-1]:
-                    v = ord(letter) - 97 
-                    if v >= 0 and v < 26:
-                        a += letter
-                ans[-1] = a
-                print(" ".join(ans), end="")
+        # if plummbing_flag == "p":
+        blob_id = sys.argv[3]
+        blob_dir = "".join(blob_id[:2])
+        path = ".git/objects/" + blob_dir + "/" + "".join(blob_id[2:])
+        with open(path, "rb") as f:
+            decompress_blob = str(zlib.decompress(f.read()))
+            vals = decompress_blob.split(" ")
+            ans = []
+            for idx, val in enumerate(vals):
+                if "\\x00" in val:
+                    vs = val.split("\\x00")
+                    ans.append(vs[-1])
+                    ans += vals[idx+1:]
+                    break
+            a = ""
+            for letter in ans[-1]:
+                v = ord(letter) - 97 
+                if v >= 0 and v < 26:
+                    a += letter
+            ans[-1] = a
+            print(" ".join(ans), end="")
     elif command == "hash-object":
         plummbing_flag = sys.argv[2]
         file_name = sys.argv[3]
