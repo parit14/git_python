71414b4ed83ad10da38808eaef711afe77d57ab6
parit14 <toshiekaushik@gmail.com>
2024-07-30 17:24:51 -0700

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import zlib
+import hashlib
 
 def main():
     # print("Logs from your program will appear here!")
@@ -15,28 +16,40 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         plummbing_flag = sys.argv[2]
-        blob_id = sys.argv[3]
-        blob_dir = "".join(blob_id[:2])
-        path = ".git/objects/" + blob_dir + "/" + "".join(blob_id[2:])
-        blob_object = open(path)
-        with open(path, "rb") as f:
-            decompress_blob = str(zlib.decompress(f.read()))
-            vals = decompress_blob.split(" ")
-            ans = []
-            for idx, val in enumerate(vals):
-                if "\\x00" in val:
-                    vs = val.split("\\x00")
-                    ans.append(vs[-1])
-                    ans += vals[idx+1:]
-                    break
-            a = ""
-            for letter in ans[-1]:
-                v = ord(letter) - 97 
-                if v >= 0 and v < 26:
-                    a += letter
-            ans[-1] = a
-            print(" ".join(ans), end="")
-    
+        if plummbing_flag == "p":
+            blob_id = sys.argv[3]
+            blob_dir = "".join(blob_id[:2])
+            path = ".git/objects/" + blob_dir + "/" + "".join(blob_id[2:])
+            with open(path, "rb") as f:
+                decompress_blob = str(zlib.decompress(f.read()))
+                vals = decompress_blob.split(" ")
+                ans = []
+                for idx, val in enumerate(vals):
+                    if "\\x00" in val:
+                        vs = val.split("\\x00")
+                        ans.append(vs[-1])
+                        ans += vals[idx+1:]
+                        break
+                a = ""
+                for letter in ans[-1]:
+                    v = ord(letter) - 97 
+                    if v >= 0 and v < 26:
+                        a += letter
+                ans[-1] = a
+                print(" ".join(ans), end="")
+    elif command == "hash-object":
+        plummbing_flag = sys.argv[2]
+        file_name = sys.argv[3]
+        with open(file_name, "r") as f:
+            content = f.read()
+            blob_header = "blob " + str(len(content)) + "\\0" + content
+            hash_content = hashlib.sha1(blob_header).hexdigest()
+            compressed_blob = zlib.compress(hash_content)
+            directory_path = ".git/objects" +"/" + hash_content[2:]
+            os.mkdir(directory_path)
+            file_path = directory_path + "/" + file_name
+            with open(file_path, "w") as f:
+                f.write(compressed_blob)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
